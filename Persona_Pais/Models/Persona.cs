fc39d4f4572f9275d7f7633f1cacf5189c5ed0b2
yohanser familia <nask9417@gmail.com>
2024-12-05 21:23:44 +0100

    ya agregamos dos modelos lo que estaremos usando en este proyecto

---
+++ Persona_Pais/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace Persona_Pais.Models
+{
+    public class Persona
+    {
+        public int IdPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+    }
+}
